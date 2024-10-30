2da749289c30a6c04002b5457f0ff236333d5a53
Alef-0 <alefgabryel123@gmail.com>
2024-10-30 09:11:56 -0300

    Finally mande it work on windows

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Ifleet_grabber
 Using Selenium to use the IFLEET on the recorder to get the camera images
+
+You will need to use EDGE with internet explorer compatibilities.
+For this to might work please see if all the registry keys are correct. using the comments of the code and the print of the documentation.
+
+- The process of sending keys is very slow, so will be sending them copying and pasting from the board.
+- Don't save passwords and other things on the code, use a .env file.
--- broser_compatible.py
+++ broser_compatible.py
@@ -1,49 +1,74 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
-# from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pyperclip as pc
+
 
 import time
 import os
 from os import path
 
-msedgewebdriver = "webdriver\\IEDriverServer32.exe"
-final_path = path.join(os.getcwd(), msedgewebdriver)
+# Probably won't need to specify but let it here
+# msedgewebdriver = "webdriver\\IEDriverServer32.exe"
+# final_path = path.join(os.getcwd(), msedgewebdriver)
 # webdriver.ie.driver_path = final_path
 
 def setup_edge_ie_mode():
-    options = webdriver.IeOptions()
-    
-    # Para não dar problemas
-    # options.add_additional_option("ie.edgechromium", True)
-    # options.add_additional_option("ie.edgepath",final_path)
-    options.ignore_zoom_level = True
-    options.attach_to_edge_chrome = True
-    # options.ignore_protected_mode_settings = True
+    ie_options = webdriver.IeOptions()
+    # So many options to work °_°
+    ie_options.ignore_protected_mode_settings = True
+    ie_options.ignore_zoom_level = True
+    # ie_options.require_window_focus = True
+    # ie_options.native_events = False
+    ie_options.ensure_clean_session = True
+    # ie_options.persistent_hover = True
+    # To use CreateProcess() with Internet Explorer 8 or higher, 
+    # the value of registry setting in HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\TabProcGrowth 
+    # must be '0'; SO CREATE IT
+    # ie_options.force_create_process_api = True
+    # There also might be other problems, read it here https://www.selenium.dev/documentation/ie_driver_server/
 
-    driver = webdriver.Ie(options=options)
-    driver.set_page_load_timeout(30)
+    # Creating options
+    driver = webdriver.Ie(options=ie_options)
+    # driver.set_script_timeout(30)
+    # driver.implicitly_wait(10)
+    driver.set_page_load_timeout(5)
+
     return driver
 
 def main():
+    driver = setup_edge_ie_mode() # Get the driver
+    waiter = WebDriverWait(driver, 10)
     try:
-        driver = setup_edge_ie_mode() # Get the driver
         driver.get("https://www.google.com.br")
-        driver.get("https://www.python.org") # Navigate to your website
+        search_box = driver.find_element(By.NAME, "q")
+        pc.copy("teste")
+        search_box.send_keys(Keys.CONTROL, 'v')
+        search_box.send_keys(Keys.ENTER)
         
-        print("CHEGOU AQUI")
-        text = driver.find_element(By.ID, "id-search-field")
-        text.send_keys("HELLO WORLD")
-        text.send_keys(Keys.RETURN)
-        time.sleep(5)
-    except Exception as e:
-        print(e)
-    finally:
-        driver.quit()
+        expected_url = "https://www.google.com.br/search"
+        waiter.until(EC.url_contains(expected_url))
+        driver.get("https://www.python.org")
+        search_box = driver.find_element(By.NAME, "q")
+        pc.copy("HELLO WORLD")
+        search_box.send_keys(Keys.CONTROL, 'v')
+        search_box.submit()
 
+        time.sleep(5)
+
+    except Exception as e:
+        print("DEU ERRO")
+        print(e)
+    
+    driver.quit()
     print("ACABOU")
 
-
+from dotenv import load_dotenv
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+    print(login, password)
